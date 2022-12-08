ca0cbbd9c072f5b54a34e9ede3431df9f498efdc
ferre011 <ferre011@newschool.edu>
2022-12-08 10:25:44 -0500

    final edits all

--- entries/entry9/main.js
+++ entries/entry9/main.js
@@ -8,15 +8,3 @@ button.addEventListener("click", function(event) {
   wrapper.appendChild(newItem);
 });
 
-let blobs = document.querySelectorAll(".blob");
-  let container = document.querySelector(".wrapper");
-  // place our blobs randomly
-  blobs.forEach(function (blob) {
-    blob.style.right = (100 * Math.random()) + '%';
-    blob.style.top = (100 * Math.random()) + '%';
-  });
-  container.addEventListener("click", function (event) {
-    if (event.target.classList.contains("blob")) {
-      event.target.remove();
-    }
-  });
